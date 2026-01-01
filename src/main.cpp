31315835ff3c9de85f7cdf31372ef3ba3f725df6
Ansh Kataria <ansh.kataria@student.uq.edu.au>
2026-01-01 15:08:42 +1000

    update main class to call parsePrice method

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+#include "parser.h"
+
 int main() {
+    parser parser("data/AAPL.csv");
+    std::vector<Price> prices = parser.parsePrice();
+
+    std::cout << "Parsed Data: " << std::endl;
+    for (auto price : prices) {
+        std::cout << "Date: " <<price.date << "Closing Price: "<<price.closingPrice<<std::endl;
+    }
+
     return 0;
 }
